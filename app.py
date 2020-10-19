b2d12ba6e152dc111f5aa60988b66219b3e7e2e8
Suraj Patil <patilsuraj767@gmail.com>
2020-10-19 18:15:10 +0530

    added changes

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def marzipan_index():
     greeting = os.environ.get('GREETING', 'Hi!')
-    return f'{greeting} You just need almond paste, powdered sugar, and egg whites.\n'
+    return f'{greeting} This is test app\n'
