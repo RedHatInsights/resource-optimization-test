6786ecb02743b0e91754218db7fe4e27f3b19b45
Suraj Patil <patilsuraj767@gmail.com>
2020-12-30 20:31:06 +0530

    testing report_consumer from main repo

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# resource-optimization-test
+# ROS-backend
+
+Backend for Resource Optimization Service
+
+## Getting Started
+
+This project uses pipenv to manage the development and production environments.
+
+Once you have pipenv installed, do the following:
+
+```bash
+pipenv install
+```
+Afterwards you can activate the virtual environment by running:
+```bash
+pipenv shell
+```
+
+## Initialize the database
+Run the following commands to excute the db migration scripts.
+```bash
+python manage.py db upgrade
+```
+
+## Running the server locally
+
+```bash
+python run.py
+```
---
+++ ros/config.py
@@ -0,0 +1,17 @@
+import os
+
+DB_USER = os.getenv("ROS_DB_USER", "postgres")
+DB_PASSWORD = os.getenv("ROS_DB_PASS", "postgres")
+DB_HOST = os.getenv("ROS_DB_HOST", "localhost")
+DB_PORT = os.getenv("ROS_DB_PORT", "15432")
+DB_NAME = os.getenv("ROS_DB_NAME", "postgres")
+DB_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}"\
+                f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+INVENTORY_HOST = os.getenv("INVENTORY_HOST", "localhost")
+INVENTORY_PORT = os.getenv("INVENTORY_PORT", "8081")
+INVENTORY_ADDRESS = f"http://{INVENTORY_HOST}:{INVENTORY_PORT}"
+
+INSIGHTS_KAFKA_HOST = os.getenv("INSIGHTS_KAFKA_HOST", "localhost")
+INSIGHTS_KAFKA_PORT = os.getenv("INSIGHTS_KAFKA_PORT", "29092")
+INSIGHTS_KAFKA_ADDRESS = f"{INSIGHTS_KAFKA_HOST}:{INSIGHTS_KAFKA_PORT}"
